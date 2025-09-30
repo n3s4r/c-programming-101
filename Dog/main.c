e529eed0d1d46ad4b30118c5ffb996ca9cf83e8d
n3s4r <141773812+n3s4r@users.noreply.github.com>
2025-09-30 17:49:12 +0600

    Add files via upload

---
+++ Dog/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Answer: %d\n", max(40, 20, 30));
+    return 0;
+}
+
+
+int max(int num1, int num2, int num3)
+{
+    int result;
+
+    if(num1 >= num2 && num1 >= num3)
+    {
+        result = num1;
+    }
+    else if(num2 >= num1 && num2 >= num3)
+    {
+        result = num2;
+    }
+    else
+    {
+        result = num3;
+    }
+    return result;
+}
